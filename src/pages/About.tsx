efcfce1f5db7f5c0e17348f9863d28822d0f27db
JeremyDelorme <jeremydelorme23.7@gmail.com>
2023-11-07 00:55:51 -0500

    brouillon du design

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
-import { Container, Typography, Paper } from '@mui/material';
+import { Typography, Box, Container, Paper } from '@mui/material';
+import cartoon_1 from '../images/cartoon_1.png';
+import cartoon_2 from '../images/cartoon_2.jpeg';
+import cartoon_3 from '../images/cartoon_3.png';
 
 const About: React.FC = () => {
     return (
         <Paper elevation={3} style={{ background: 'linear-gradient(90deg, #ff9566, #ff58c7)', padding: '16px' }}>
-            <Container maxWidth="sm">
-                <Typography variant="h2" gutterBottom style={{ fontFamily: 'cursive', color: '#fff' }}>
-                    About the 70's
-                </Typography>
-                <Typography variant="body1" style={{ fontFamily: 'cursive', color: '#fff' }}>
-                    Learn more about the groovy 70's, the music, fashion, and the culture.
-                </Typography>
-            </Container>
+            <Box style={{ paddingLeft: '100px', paddingTop: '100px', marginTop: '30px', color: '#FFFBF' }}>
+                <Container className="content-container">
+                    <Typography variant="h2" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                        À propos de moi
+                    </Typography>
+
+                    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" my={4}>
+                        <Typography variant="h6" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quae dolores ab facilis ullam, perspiciatis animi nemo repellat sunt quis sint saepe deserunt! Rem similique eveniet molestias nemo adipisci. Est?
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" alignItems="center" justifyContent="space-around" my={4}>
+                        <Box mx={4} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                            <img src={cartoon_1} alt="Image 1" style={{ maxWidth: '100%' }} />
+                            <Typography variant="h6" style={{ marginTop: '8px', fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                            </Typography>
+                        </Box>
+
+                        <Box mx={4} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                            <img src={cartoon_2} alt="Image 2" style={{ maxWidth: '100%' }} />
+                            <Typography variant="h6" style={{ marginTop: '8px', fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                                quae dolores ab facilis ullam, perspiciatis animi nemo repellat
+                            </Typography>
+                        </Box>
+
+                        <Box mx={4} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                            <img src={cartoon_3} alt="Image 3" style={{ maxWidth: '100%' }} />
+                            <Typography variant="h6" style={{ marginTop: '8px', fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                                sunt quis sint saepe deserunt! Rem similique eveniet molestias nemo adipisci. Est?
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Container>
+            </Box>
         </Paper>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Typography, Button, CssBaseline, Grid, Box, Container } from '@mui/material';
+import { Typography, Button, CssBaseline, Grid, Box, Container, Paper } from '@mui/material';
 import heroImage from '../images/heroImage.svg';
 import formes from '../images/formes.svg';
 import { makeStyles } from '@material-ui/core/styles';
+
+import cartoon_1 from '../images/cartoon_1.png';
+import cartoon_2 from '../images/cartoon_2.jpeg';
+import cartoon_3 from '../images/cartoon_3.png';
 
 const useStyles = makeStyles((theme) => ({
     '@global': {
@@ -13,6 +17,20 @@ const useStyles = makeStyles((theme) => ({
             fontFamily: 'Josefin Slab Thin, sans-serif',
         },
     },
+    contentContainerAbout: {
+        paddingTop: '400px',
+        width: '50%'
+
+    },
+    contentContainerProjects: {
+        paddingTop: '600px',
+    },
+
+    contentContainerContact: {
+        paddingTop: '1100px',
+        marginBottom: '100px'
+    },
+
     contentContainer: {
         display: 'flex',
         flexDirection: 'column',
@@ -22,10 +40,9 @@ const useStyles = makeStyles((theme) => ({
     },
     leftContent: {
         display: 'flex',
-        justifyContent: 'center',
         flexDirection: 'column',
         alignItems: 'center',
-        paddingTop: '50px'
+        paddingTop: '50px',
     },
     rightContent: {
         display: 'flex',
@@ -49,9 +66,7 @@ const useStyles = makeStyles((theme) => ({
         backgroundRepeat: 'no-repeat',
         backgroundAttachment: 'scroll',
         backgroundPosition: 'center top', // Center the background image horizontally and vertically
-
     },
-
 }));
 
 const Home: React.FC = () => {
@@ -62,62 +77,85 @@ const Home: React.FC = () => {
             <CssBaseline />
             <Box className={classes.outerContainer}>
                 <Container maxWidth="xl" className={classes.contentContainer}>
-                    <Grid container justifyContent="space-between">
+                    <Grid container justifyContent="center">
                         <Grid item xs={12} md={6} className={classes.leftContent}>
                             <img src={heroImage} alt="Your Image Description" className={classes.stackedImage} />
                         </Grid>
                         <Grid item xs={12} md={6} className={classes.rightContent}>
                             <div className={classes.textStack}>
-                                <Typography variant="h3" gutterBottom style={{ paddingTop: '200px', paddingLeft: '40px', fontFamily: 'Josefin Slab Thin, sans-serif' }}>
+                                <Typography variant="h3" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif' }}>
                                     Développeur Web Full-Stack
                                 </Typography>
-                                <Typography style={{ paddingLeft: '40px', fontFamily: 'Josefin Slab Thin, sans-serif' }} variant="h5">
+                                <Typography variant="h5" style={{ fontFamily: 'Josefin Slab Thin, sans-serif' }}>
                                     De beaux sites webs
                                 </Typography>
-                                <Button variant="contained" color="primary" style={{ marginLeft: '40px', marginTop: '100px', fontFamily: 'Josefin Slab Thin, sans-serif' }}>
+                                <Button variant="contained" color="primary" style={{ marginTop: '40px', fontFamily: 'Josefin Slab Thin, sans-serif' }}>
                                     Davantage
                                 </Button>
                             </div>
                         </Grid>
                     </Grid>
                 </Container>
-                <Box style={{ marginTop: '30px', width: '100%' }}>
-                    <Container maxWidth="sm">
-                        <Typography variant="h2" gutterBottom style={{ fontFamily: 'cursive', color: '#fff' }}>
-                            About the 70's
+
+                <Container className={classes.contentContainerAbout}>
+                    <Box>
+
+                    </Box>
+                    <Typography variant="h2" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                        À propos de moi
+                    </Typography>
+
+                    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" my={4}>
+                        <Typography variant="h6" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quae dolores ab facilis ullam, perspiciatis animi nemo repellat sunt quis sint saepe deserunt! Rem similique eveniet molestias nemo adipisci. Est? Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quae dolores ab facilis ullam, perspiciatis animi nemo repellat sunt quis sint saepe deserunt! Rem similique eveniet molestias nemo adipisci. Est? Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quae dolores ab facilis ullam, perspiciatis animi nemo repellat sunt quis sint saepe deserunt! Rem similique eveniet molestias nemo adipisci. Est?
                         </Typography>
-                        <Typography variant="body1" style={{ fontFamily: 'cursive', color: '#fff' }}>
-                            Learn more about the groovy 70's, the music, fashion, and the culture.
+                    </Box>
+                </Container>
+
+                <Container className={classes.contentContainerProjects}>
+                    <Box>
+                        <Typography variant="h2" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                            Mes projets
                         </Typography>
+                    </Box>
 
-                        <Box display="flex" alignItems="center" justifyContent="center" my={4}>
-                            <Box textAlign="center">
-                                <Typography variant="h3" gutterBottom>
-                                    H3 Text
-                                </Typography>
-                                <Typography variant="h5">
-                                    H5 Text
-                                </Typography>
-                            </Box>
-
-                            <Box mx={4} display="flex" flexDirection="column" alignItems="center">
-                                <img src="image1.jpg" alt="Image 1" style={{ maxWidth: '100%' }} />
-                                <Typography variant="h6" style={{ marginTop: '8px' }}>H6 Text 1</Typography>
-                            </Box>
-
-                            <Box mx={4} display="flex" flexDirection="column" alignItems="center">
-                                <img src="image2.jpg" alt="Image 2" style={{ maxWidth: '100%' }} />
-                                <Typography variant="h6" style={{ marginTop: '8px' }}>H6 Text 2</Typography>
-                            </Box>
-
-                            <Box mx={4} display="flex" flexDirection="column" alignItems="center">
-                                <img src="image3.jpg" alt="Image 3" style={{ maxWidth: '100%' }} />
-                                <Typography variant="h6" style={{ marginTop: '8px' }}>H6 Text 3</Typography>
-                            </Box>
+                    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" my={4}>
+                        <Typography variant="h6" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quae dolores ab facilis ullam, perspiciatis animi nemo repellat sunt quis sint saepe deserunt! Rem similique eveniet molestias nemo adipisci. Est?
+                        </Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" justifyContent="center" my={4}>
+                        <Box mx={4} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                            <img src={cartoon_1} alt="Image 1" style={{ maxWidth: '100%', height: '400px' }} />
+                            <Typography variant="h6" style={{ marginTop: '8px', fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                            </Typography>
                         </Box>
-                    </Container>
-                </Box>
+                        <Box mx={4} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                            <img src={cartoon_2} alt="Image 2" style={{ maxWidth: '100%', height: '400px' }} />
+                            <Typography variant="h6" style={{ marginTop: '8px', fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                                quae dolores ab facilis ullam, perspiciatis animi nemo repellat
+                            </Typography>
+                        </Box>
+                        <Box mx={4} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                            <img src={cartoon_3} alt="Image 3" style={{ maxWidth: '100%', height: '400px' }} />
+                            <Typography variant="h6" style={{ marginTop: '8px', fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                                sunt quis sint saepe deserunt! Rem similique eveniet molestias nemo adipisci. Est?
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Container>
 
+                <Container className={classes.contentContainerContact}>
+                    <Typography variant="h2" gutterBottom style={{ fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                        Contact
+                    </Typography>
+                    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" my={4}>
+                        <Typography variant="h6" gutterBottom style={{ marginTop: '50px', fontFamily: 'Josefin Slab Thin, sans-serif', textAlign: 'center' }}>
+                            jeremydelormeweb@gmail.com
+                        </Typography>
+                    </Box>
+                </Container>
             </Box>
         </>
     );
